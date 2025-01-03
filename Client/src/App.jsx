f8dd4048590cfc590338eff3d6456f0c1601bf00
vikash kumar <rajsoniabhishek@gmail.com>
2025-01-03 15:26:51 +0530

    push the code of navbar

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,9 @@
+import Navbar from "./component/Navbar";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello  vikash 
-    </h1>
+       <div>
+       <Navbar />
+       </div>
   )
 }
